c119260ac8dc10a0baca7e532e61d04acc3ebb82
sa.hyll <mohdasahil28@gmail.com>
2024-04-25 12:58:33 +0200

    Added Suspense from react

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,6 +1,6 @@
 'use client';
 
-import {useEffect, useState} from 'react';
+import {useEffect, useState, Suspense} from 'react';
 import { useRouter , useSearchParams} from 'next/navigation';
 
 import Form from '@components/Form'; //
